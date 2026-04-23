a22d66dd9bc3c0008928b15bb1bd929929e2a4b8
Jacob <jfhannestad@gmail.com>
2026-04-23 10:11:46 +0200

    Gjorde oppgave 2 av testing

---
+++ applikasjon.py
@@ -0,0 +1,11 @@
+# def kvadratTall(tall):
+#     return tall ** 2
+
+def addisjon(a, b): 
+    return a + b 
+
+def subtraksjon(a, b):
+    return a - b
+
+def multiplikasjon(a, b):
+    return a * b
